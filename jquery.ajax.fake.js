caaa7f529b13d3481ef927634dd5768015d0279c
anasnakawa <anas.nakawa@gmail.com>
2013-03-09 02:29:46 +0400

    tabs into spaces

--- jquery.ajax.fake.js
+++ jquery.ajax.fake.js
@@ -6,69 +6,69 @@
  */
 // ------------------------------
 // author : Anas Nakawa
-//			anas.nakawa@gmail.com
-//			@anasnakawa
+//      anas.nakawa@gmail.com
+//      @anasnakawa
 // ------------------------------
 
 (function($){
 
-	// caching original jquery ajax method
-	var ajax = $.ajax
-	, fakeWebServices = {}
-	, deferred = $.Deferred()
-	, defaults = {
-		fake	: false	// is it fake ?
-		, wait	: 1000	// how long should wait before return ajax response 
-	}
-	
-	, ajaxFake = function(options) {
-		
-		// not fake, just return the original jquery ajax
-		if( $.ajax.isFake === false ) {
-		  return ajax.apply(this, arguments);
-		}
-		
-		if( !options.fake ) {
-		  return ajax.apply(this, arguments);
-		}
-		
-		options = $.extend(defaults, options);
-		
-		if( !fakeWebServices[options.url] ) {
-			$.error('{url} 404 not found'.replace(/{url}/, options.url));
-			return deferred.reject('404');
-		}
+  // caching original jquery ajax method
+  var ajax = $.ajax
+  , fakeWebServices = {}
+  , deferred = $.Deferred()
+  , defaults = {
+    fake  : false  // is it fake ?
+    , wait  : 1000  // how long should wait before return ajax response 
+  }
+  
+  , ajaxFake = function(options) {
+    
+    // not fake, just return the original jquery ajax
+    if( $.ajax.isFake === false ) {
+      return ajax.apply(this, arguments);
+    }
+    
+    if( !options.fake ) {
+      return ajax.apply(this, arguments);
+    }
+    
+    options = $.extend(defaults, options);
+    
+    if( !fakeWebServices[options.url] ) {
+      $.error('{url} 404 not found'.replace(/{url}/, options.url));
+      return deferred.reject('404');
+    }
 
-		// fake it..
-		setTimeout(function() {
-			var data = fakeWebServices[options.url](options.data);
-			if(options.success) {
-				options.success( data );
-			}
-			if(options.complete) {
-			  options.complete( data );
-			}
-			deferred.resolve( data );
-			
-		}, options.wait);
-		
-		// return a promise object
-		return deferred.promise();
-	}
-	
-	, registerFakeWebService = function(url, callback) {
-		fakeWebServices[url] = function(data) {
-			return callback(data);
-		}
-	}
-	
-	// expose to jquery api
-	// --------------------
-	$.ajax = ajaxFake;
-	$.ajax.fake = {
-		defaults				      : defaults
-		, registerWebservice	: registerFakeWebService
-		, webServices			    : fakeWebServices
-	};
+    // fake it..
+    setTimeout(function() {
+      var data = fakeWebServices[options.url](options.data);
+      if(options.success) {
+        options.success( data );
+      }
+      if(options.complete) {
+        options.complete( data );
+      }
+      deferred.resolve( data );
+      
+    }, options.wait);
+    
+    // return a promise object
+    return deferred.promise();
+  }
+  
+  , registerFakeWebService = function(url, callback) {
+    fakeWebServices[url] = function(data) {
+      return callback(data);
+    }
+  }
+  
+  // expose to jquery api
+  // --------------------
+  $.ajax = ajaxFake;
+  $.ajax.fake = {
+    defaults              : defaults
+    , registerWebservice  : registerFakeWebService
+    , webServices          : fakeWebServices
+  };
 
 })(jQuery);
